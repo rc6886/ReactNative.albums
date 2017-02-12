f377b26f0a293a5af5aa1fb9491319aecf623c29
Rodney Coleman <rc6886@gmail.com>
2017-02-12 11:22:35 -0600

    Adding finished albums app.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    Linking
+} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle,
+    } = styles;
+
     return (
         <Card>
             <CardSection>
-                <View></View>
-                <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        source={{ uri: thumbnail_image }}
+                        style={thumbnailStyle}
+                    />
                 </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image
+                    source={{ uri: image }}
+                    style={imageStyle}
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
@@ -20,8 +53,26 @@ const AlbumDetail = (props) => {
 const styles = {
     headerContentStyle: {
         flexDirection: 'column',
-        justifyContent: 'space-around'
-    }
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+      fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+    },
 };
 
 export default AlbumDetail;
